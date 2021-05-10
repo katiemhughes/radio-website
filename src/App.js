2947a9a0f829fa1f1021004ae2b06ea42408c98b
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-05-10 22:51:15 +0100

    deleted unneeded router variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 // import Navbar from './components/navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 // import Home from "./components/home/Home";
 // import Genres from './components/genres/Genres';
 import ComingSoon from './components/comingSoon/ComingSoon';
