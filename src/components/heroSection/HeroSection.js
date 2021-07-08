9bb7ed3a8780c0495731d9f58200782a23a66e9c
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-08 21:30:48 +0100

    Passed logo in as prop

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./HeroSection.css";
+import "../home/Data";
 
-function HeroSection() {
+const HeroSection = ({ topLine, img, alt }) => {
     return (
         <div className="hero-container">
             {/* <video src="/videos/rain-video.mp4" autoPlay loop muted /> */}
-            <img src="/images/mainlogo.png" alt="Main logo" className="main-logo" />
-            <h1>Broadcasting from an undisclosed location in south Manchester</h1>
+            <img src={img} alt={alt} className="main-logo" />
+            <h1>{topLine}</h1>
         </div>
     )
 }
