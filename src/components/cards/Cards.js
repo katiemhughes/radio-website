aa7d3ad4e16e09a357745ef92165db576ff3b141
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-08 21:27:53 +0100

    Added new card examples and sizing

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,47 +1,48 @@
 import React from 'react';
 import CardItem from "../cards/CardItem";
 import "../cards/Cards.css"
+import "../home/Data";
 
-function Cards() {
+const Cards = () => {
     return (
-        <div className="cards">
-            <h1 className="cards__title">RCR Picks</h1>
+        <div className="cards" id="picks">
+            <h1 className="cards__title">RCR PICKS</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src="images/sherelle.jpg"
-                            alt="Sherelle"
-                            djName="SHERELLE"
+                            src="images/pussy-stamina.jpg"
+                            alt="Pussy Stamina"
+                            djName="PUSSY STAMINA"
                             date="05 May 2021"
-                            genres="160"
+                            genres="Club"
                             label="Jungle"
                             path="/genres"
                         />
                         <CardItem
-                            src="images/timreaper.jpg"
-                            alt="Tim Reaper"
-                            djName="TIM REAPER"
+                            src="images/Charlie-b.jpg"
+                            alt="Charlie B"
+                            djName="CHARLIE B"
                             date="07 May 2021"
-                            genres="Jungle"
+                            genres="Club"
                             label="Jungle"
                             path="/genres"
                         />
                         <CardItem
-                            src="images/lcy.jpg"
-                            alt="LCY"
-                            djName="LCY"
+                            src="images/dig-b.jpg"
+                            alt="Dig B"
+                            djName="Dig-B"
                             date="10 May 2021"
                             genres="Bass"
                             label="Jungle"
                             path="/genres"
                         />
                         <CardItem
-                            src="images/ahadadream.jpg"
-                            alt="Ahadadream"
-                            djName="AHADADREAM"
+                            src="images/maracuya.jpg"
+                            alt="Maracuya"
+                            djName="MARACUYA"
                             date="15 May 2021"
-                            genres="UK Funky"
+                            genres="Bass"
                             label="Jungle"
                             path="/genres"
                         />
