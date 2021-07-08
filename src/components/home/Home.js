39d80dc29bc3004ac1c9eb8d1e422e69adc4ee0f
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-08 21:31:16 +0100

    Added more components into home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import HeroSection from "../heroSection/HeroSection";
 import Cards from "../cards/Cards";
+import Schedule from "../schedule/Schedule";
+import About from "../about/About";
+import {homeObjOne} from "./Data";
+import Footer from "../footer/Footer";
 
-function Home() {
+const Home = () => {
     return (
-        <>
-            <HeroSection />
-            <Cards />
-        </>
+        <div id="home">
+            <HeroSection {...homeObjOne} />
+            {/* <Cards /> */}
+            <Schedule />
+            <About />
+            <Footer />
+        </div>
     )
 }
 
