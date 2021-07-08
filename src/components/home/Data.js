db488969ebe8e228d5e2ac755ece4624c5c80215
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-08 21:31:05 +0100

    Added data to be passed as props

---
+++ src/components/home/Data.js
@@ -0,0 +1,10 @@
+export const homeObjOne = {
+    topLine: 'Broadcasting from an undisclosed location in south Manchester',
+    img: 'images/mainlogo.png',
+    alt: 'Main logo'
+}
+export const homeObjTwo = {
+    headLine: 'RCR Picks',
+    img: 'images/mainlogo.png',
+    alt: 'Main logo'
+}
