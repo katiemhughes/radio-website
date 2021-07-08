f36837bb1fe0308d72d287241898c9c1c782cbb2
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-08 21:27:08 +0100

    Added about page

---
+++ src/components/about/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./About.css";
+
+const About = () => {
+    return (
+        <div className="about-container" id="about">
+            <h1 className="about-title">About</h1>
+            <p className="about-text">
+                Established in July 2021 in Manchester, Rainy City Radio broadcasts out of Old Trafford to showcase sounds from the rainy city and beyond.
+
+                Our core values are to broadcast high quality music; to create a safe and inclusive environment to nurture and encourage upcoming and existing talent; and give back to the community.
+            </p>
+        </div>
+    )
+}
+
+export default About;
