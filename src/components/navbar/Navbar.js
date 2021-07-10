e6725c7aed2f027a8f7bdebec19284fb3b07905b
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-10 19:59:19 +0100

    Team GB

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -114,7 +114,7 @@ const Navbar = () => {
                 <li>ON AIR</li>
                 <div className="audio-player-container">
                   <Player />
-                  <p className="audio-player-text">Maracuya b2b Dig B</p>
+                  <p className="audio-player-text">Team GB</p>
                 </div>
               </ul>
             </div>
