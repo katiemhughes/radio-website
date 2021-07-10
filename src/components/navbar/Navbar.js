449b31da2af007c5672659110c202a58fde2c5c3
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-10 21:46:49 +0100

    Ship Sket

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -114,7 +114,7 @@ const Navbar = () => {
                 <li>ON AIR</li>
                 <div className="audio-player-container">
                   <Player />
-                  <p className="audio-player-text">Salt Pillar</p>
+                  <p className="audio-player-text">Ship Sket</p>
                 </div>
               </ul>
             </div>
