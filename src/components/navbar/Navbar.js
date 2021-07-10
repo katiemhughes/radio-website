15473c3f17afadf1042159df3449104ed313c7c1
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-11 00:01:30 +0100

    off air 10/07

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -111,10 +111,10 @@ const Navbar = () => {
             <div className="bottom-left">
               <ul>
                 <li><div className="circle red"></div></li>
-                <li>ON AIR</li>
+                <li>OFF AIR</li>
                 <div className="audio-player-container">
                   <Player />
-                  <p className="audio-player-text">Earl Grey</p>
+                  <p className="audio-player-text"></p>
                 </div>
               </ul>
             </div>
