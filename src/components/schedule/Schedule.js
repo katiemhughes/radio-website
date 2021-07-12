3cbe206dcc65ede177cb59aeb85bee7c361ce651
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-12 21:38:37 +0100

    Schedule updated

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -3,6 +3,7 @@ import "./Schedule.css";
 import "../home/Data";
 import ScheduleItem from "./ScheduleItem";
 import ScheduleDate from "./ScheduleDate";
+import ScheduleTakeover from "./ScheduleTakeover";
 
 const Schedule = () => {
     return (
@@ -10,45 +11,16 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Saturday 10th July"
+                    showDate="Thursday 15th July"
                 />
-                <ScheduleItem
+                {/* <ScheduleItem
                     showTime="16:00 - 17:00"
                     showName="Joey T"
                 />
                 <ScheduleItem
                     showTime="17:00 - 18:00"
                     showName="Ellen"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="La Presidenta"
-                />
-                <ScheduleItem
-                    showTime="19:00 - 20:00"
-                    showName="Dig B b2b Maracuya"
-                />
-                <ScheduleItem
-                    showTime="20:00 - 21:00"
-                    showName="Team GB"
-                />
-                <ScheduleItem
-                    showTime="21:00 - 22:00"
-                    showName="Salt Pillar"
-                />
-                <ScheduleItem
-                    showTime="22:00 - 23:00"
-                    showName="Ship Sket"
-                />
-                <ScheduleItem
-                    showTime="23:00 - 00:00"
-                    showName="Earl Grey"
-                />
-            </table>
-            {/* <table>
-                <ScheduleDate 
-                    showDate="Thursday 15th July"
-                />
+                /> */}
                 <ScheduleItem
                     showTime="18:00 - 19:00"
                     showName="Rhys Evans"
@@ -65,17 +37,25 @@ const Schedule = () => {
                     showTime="21:00 - 22:00"
                     showName="iz.wav"
                 />
+                {/* <ScheduleItem
+                    showTime="22:00 - 23:00"
+                    showName="Ship Sket"
+                />
+                <ScheduleItem
+                    showTime="23:00 - 00:00"
+                    showName="Earl Grey"
+                /> */}
             </table>
-            <table>
+            <table className="schedule-table">
                 <ScheduleDate 
                     showDate="Friday 16th July"
                 />
                 <ScheduleTakeover 
-                    takeoverShow="Not Bad for a Girl Takeover"
+                    takeoverShow="Not Bad for a Girl and friends"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="DaiSu"
+                    showName="Pussy Stamina"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
@@ -83,18 +63,18 @@ const Schedule = () => {
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="MOLL"
+                    showName="Maracuya"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="ThtGrl"
+                    showName="TBC"
                 />
                 <ScheduleItem
                     showTime="22:00 - 23:00"
-                    showName="Velmz"
+                    showName="MOLL"
                 />
             </table>
-            <table>
+            <table className="schedule-table">
                 <ScheduleDate 
                     showDate="Saturday 17th July"
                 />
@@ -104,15 +84,15 @@ const Schedule = () => {
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Charlie B b2b Pussy Stamina"
+                    showName="Charlie B"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Monir"
+                    showName="Mercy System fka Monir"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Mix-Stress"
+                    showName="Shannon From Admin"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
@@ -120,9 +100,9 @@ const Schedule = () => {
                 />
                 <ScheduleItem
                     showTime="22:00 - 23:00"
-                    showName="Poison Ivy"
+                    showName="TBC"
                 />
-            </table> */}
+            </table>
         </div>
     )
 }
