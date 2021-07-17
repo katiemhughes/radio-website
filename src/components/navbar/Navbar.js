b6ab251ba281fcfaf0a5bbbae7e2439842d04e2e
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-17 19:43:54 +0100

    shannon from admin

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -114,7 +114,7 @@ const Navbar = () => {
                 <li>ON AIR</li>
                 <div className="audio-player-container">
                   <Player />
-                  <p className="audio-player-text">Mercy System</p>
+                  <p className="audio-player-text">Shannon from Admin</p>
                 </div>
               </ul>
             </div>
