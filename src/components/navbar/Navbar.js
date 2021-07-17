61b4f49b9c0bde6ac6c089cfa74e2d33f1731d28
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-17 20:58:47 +0100

    kiss me again

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -114,7 +114,7 @@ const Navbar = () => {
                 <li>ON AIR</li>
                 <div className="audio-player-container">
                   <Player />
-                  <p className="audio-player-text">Shannon from Admin</p>
+                  <p className="audio-player-text">Kiss Me Again</p>
                 </div>
               </ul>
             </div>
