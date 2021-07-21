c9cace2a58f7b54d7df7116d81a737571c1206e0
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-21 19:22:31 +0100

    schedule wc 19/07

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,97 +11,73 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 15th July"
-                />
-                {/* <ScheduleItem
-                    showTime="16:00 - 17:00"
-                    showName="Joey T"
-                />
-                <ScheduleItem
-                    showTime="17:00 - 18:00"
-                    showName="Ellen"
-                /> */}
-                <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="Dig B"
+                    showDate="Thursday 22nd July"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Charlie B"
+                    showName="DaiSu"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Ben Daddy"
+                    showName="SaltPeter"
+                />
+                <ScheduleItem
+                    showTime="21:00 - 22:00"
+                    showName="Trax Haven"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
                     showName="iz.wav"
                 />
-                {/* <ScheduleItem
-                    showTime="22:00 - 23:00"
-                    showName="Ship Sket"
-                />
-                <ScheduleItem
-                    showTime="23:00 - 00:00"
-                    showName="Earl Grey"
-                /> */}
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Friday 16th July"
+                    showDate="Friday 23rd July"
                 />
                 <ScheduleTakeover 
-                    takeoverShow="Not Bad for a Girl and friends"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="Pussy Stamina"
+                    takeoverShow="Noire State Takeover"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Maracuya"
+                    showName="Insurrect"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Moll"
+                    showName="Burgess"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Mystery Set"
+                    showName="Manny P"
                 />
-                {/* <ScheduleItem
+                <ScheduleItem
                     showTime="22:00 - 23:00"
-                    showName="MOLL"
-                /> */}
+                    showName="Azusa"
+                />
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Saturday 17th July"
+                    showDate="Saturday 24th July"
                 />
                 <ScheduleItem
                     showTime="17:00 - 18:00"
-                    showName="Talking Shop"
+                    showName="Desodah"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Dig B"
+                    showName="Alfaz"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Mercy System fka Monir"
+                    showName="Ayy Den"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Shannon From Admin"
+                    showName="D. Clemente"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Kiss Me Again"
+                    showName="Kitsta"
                 />
-                {/* <ScheduleItem
-                    showTime="22:00 - 23:00"
-                    showName="TBC"
-                /> */}
             </table>
         </div>
     )
