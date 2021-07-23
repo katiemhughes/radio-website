c5ec32fc1d11a64b2aa5ce902b2d2768476bf27f
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-23 18:46:03 +0100

    insurrect

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text"></p>
+                  <p className="audio-player-text">Insurrect</p>
                 </div>
               </ul>
             </div>
