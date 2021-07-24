66c96fb10f3725e0d42ca944376d5088ce9b5fb8
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-07-24 20:53:26 +0100

    kitsta

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">d. clemente</p>
+                  <p className="audio-player-text">kitsta</p>
                 </div>
               </ul>
             </div>
