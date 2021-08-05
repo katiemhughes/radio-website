526191d4b988d23a9a374a6b24bae84faf770737
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-05 19:53:55 +0100

    Izzy Iz

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Motor City Donk Ensemble</p>
+                  <p className="audio-player-text">Izzy Iz</p>
                 </div>
               </ul>
             </div>
