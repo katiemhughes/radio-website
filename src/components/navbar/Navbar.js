166eb99998e26aa55bb25002d46bdf943aee4555
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-06 22:00:27 +0100

    p45 b2b rainy city

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Sozaboi</p>
+                  <p className="audio-player-text">P45 b2b Rainy City</p>
                 </div>
               </ul>
             </div>
