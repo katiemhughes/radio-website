21eb9daac61d62c9fabf63b509ac0695dbe1a85d
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-06 19:55:19 +0100

    skiptrace djs

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Mystique</p>
+                  <p className="audio-player-text">Skiptrace DJs</p>
                 </div>
               </ul>
             </div>
