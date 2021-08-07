698fd440d59a0bd518a21e57c7bb2e94327e8026
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-07 22:47:09 +0100

    closing set

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">DJ Simcard b2b Harry Amstel</p>
+                  <p className="audio-player-text">Closing set</p>
                 </div>
               </ul>
             </div>
