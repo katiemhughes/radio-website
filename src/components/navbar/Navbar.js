c455ed8cfdc49da5c9427b0649b75ad61181277d
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-07 20:55:20 +0100

    ben daddy

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Moose</p>
+                  <p className="audio-player-text">Ben Daddy</p>
                 </div>
               </ul>
             </div>
