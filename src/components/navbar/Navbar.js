5c7efc83b8ffd28649e450301f79a8e1cde23456
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-07 22:47:45 +0100

    closing set

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Closing set</p>
+                  <p className="audio-player-text">Closing Set</p>
                 </div>
               </ul>
             </div>
