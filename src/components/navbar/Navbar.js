cb26120782ce8adc2414ceec1046d2e0ecdc6ce6
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-07 21:48:26 +0100

    harry amstel dj simcard

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Ben Daddy</p>
+                  <p className="audio-player-text">DJ Simcard b2b Harry Amstel</p>
                 </div>
               </ul>
             </div>
