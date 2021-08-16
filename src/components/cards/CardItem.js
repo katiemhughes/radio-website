862dd9262edfcc0e12ff065e97dce3f75fcebf3c
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-16 21:35:54 +0100

    updated genre buttons

--- src/components/cards/CardItem.js
+++ src/components/cards/CardItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { Link } from "react-router-dom";
+// import  { Link } from "react-router-dom";
 import Button from '../genretags/GenreTags';
 import "../home/Data";
 
@@ -7,17 +7,33 @@ const CardItem = (props) => {
     return (
         <>
             <li className="cards__items--item">
-                <Link className="cards__items--link" to={props.path}>
-                    <img src={props.src} alt={props.alt} className="cards__items--img" />
-                    <div className="cards__items--info">
-                        <h1 className="dj-title">{props.djName}</h1>
-                        <h5 className="cards__items--text">{props.date}</h5>
-                        <Button
-                            onClick={() => {console.log("You clicked on me")}}
-                            type="button"
-                            >{props.genres}</Button>
+                {/* <Link className="cards__items--link" to={props.path}> */}
+                <a href={props.showLink} target="_blank">
+                    <div className="cards__items--link">
+                        <img src={props.src} alt={props.alt} className="cards__items--img" />
+                        <div className="cards__items--info">
+                            <h1 className="dj-title">{props.djName}</h1>
+                            <h5 className="cards__items--text">{props.date}</h5>
+                            <section className="buttons-container">
+                                <Button
+                                    // onClick={() => {console.log("You clicked on me")}}
+                                    type="button"
+                                    >{props.genre1}
+                                </Button>
+                                <Button
+                                    // onClick={() => {console.log("You clicked on me")}}
+                                    type="button"
+                                    >{props.genre2}
+                                </Button>
+                                <Button
+                                    // onClick={() => {console.log("You clicked on me")}}
+                                    type="button"
+                                    >{props.genre3}
+                                </Button>
+                            </section>
+                        </div>
                     </div>
-                </Link>
+                </a>
             </li>
         </>
     )
