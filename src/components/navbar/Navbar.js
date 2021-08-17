64e442f9d48cd05e67114d4a83faf81b8e6d7ffd
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-17 23:30:42 +0100

    moved nav items around

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -45,20 +45,6 @@ const Navbar = () => {
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="schedule"
-                    onClick={closeMobileMenu}
-                    smooth={true}
-                    duration={500}
-                    spy={true}
-                    exact="true"
-                    offset={-80}
-                    activeClass="active">
-                      Schedule
-                  </Link>
-                </li>
-                <li className="nav-item">
                   <Link 
                     className="nav-link" 
                     to="picks" 
@@ -70,6 +56,20 @@ const Navbar = () => {
                     offset={-80}
                     activeClass="active">
                       Picks
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="schedule"
+                    onClick={closeMobileMenu}
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    activeClass="active">
+                      Schedule
                   </Link>
                 </li>
                 {/* <li className="nav-item">
