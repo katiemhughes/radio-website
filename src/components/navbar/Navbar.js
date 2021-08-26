3283a4b165a26296224f08cd40e6933e9c45ec93
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-26 18:50:38 +0100

    sweet nothings w/ dig b

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">T.T.E.</p>
+                  <p className="audio-player-text">Sweet nothings w/ dig B</p>
                 </div>
               </ul>
             </div>
