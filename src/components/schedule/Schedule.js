0d252c89f627ffa070c660dcfa5f88a59de37c1e
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-26 17:30:14 +0100

    schedule wc 23/08

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,73 +11,61 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 12th August"
+                    showDate="Thursday 26th August"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Places and Spaces"
+                    showName="T.T.E."
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Fred Salt"
+                    showName="Sweet Nothings"
                 />
                 <ScheduleItem
-                    showTime="20:00 - 21:00"
-                    showName="Lev"
-                />
-                <ScheduleItem
-                    showTime="21:00 - 22:00"
-                    showName="Elia"
+                    showTime="20:00 - 22:00"
+                    showName="Alex Milo b2b Yorlig"
                 />
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Friday 13th August"
+                    showDate="Friday 27th August"
                 />
                 <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="Miss Ridd"
-                />
-                <ScheduleItem
-                    showTime="19:00 - 20:00"
-                    showName="Hanz"
+                    showTime="18:00 - 20:00"
+                    showName="DaiSu b2b Kitsta"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Bicho Raro"
+                    showName="Ricky Chong"
                 />
                 <ScheduleItem
-                    showTime="21:00 - 22:00"
-                    showName="Egg On Toast"
+                    showTime="21:00 - 23:00"
+                    showName="Under the Umbrella"
                 />
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Saturday 14th August"
+                    showDate="Saturday 28th August"
                 />
                 <ScheduleItem
                     showTime="17:00 - 18:00"
-                    showName="Polly (Loose Fit)"
+                    showName="Mephisto"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Lo5ive (Kultura Collective)"
+                    showName="Thtgrl"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Pod Imiya"
+                    showName="Laura Hancock"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Chandé"
+                    showName="Maanta"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Hardcore Soy"
-                />
-                <ScheduleItem
-                    showTime="22:00 - 23:00"
-                    showName="Cam Spen"
+                    showName="Seren Seo"
                 />
             </table>
         </div>
