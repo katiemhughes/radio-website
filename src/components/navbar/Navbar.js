3f3b9ee6b54829928745feb0c8e35cc74aa9070d
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-08-28 19:54:54 +0100

    pussy stamina

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Laura Hancock</p>
+                  <p className="audio-player-text">Pussy Stamina</p>
               
                 </div>
               </ul>
