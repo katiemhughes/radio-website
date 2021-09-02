19a422563a8df978fdec0e8131b2202a7b5a5d80
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-09-02 13:36:53 +0100

    schedule wc 02/09

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,61 +11,73 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 26th August"
+                    showDate="Thursday 2nd September"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="T.T.E."
+                    showName="Acidhousedeathsquad"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Sweet Nothings"
-                />
-                <ScheduleItem
-                    showTime="20:00 - 22:00"
-                    showName="Alex Milo b2b Yorlig"
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Friday 27th August"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 20:00"
-                    showName="DaiSu b2b Kitsta"
+                    showName="Curl FM"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Ricky Chong"
-                />
-                <ScheduleItem
-                    showTime="21:00 - 23:00"
-                    showName="Under the Umbrella"
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Saturday 28th August"
-                />
-                <ScheduleItem
-                    showTime="17:00 - 18:00"
-                    showName="Mephisto"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="Thtgrl"
-                />
-                <ScheduleItem
-                    showTime="19:00 - 20:00"
-                    showName="Laura Hancock"
-                />
-                <ScheduleItem
-                    showTime="20:00 - 21:00"
-                    showName="Pussy Stamina"
+                    showName="Paryah"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Seren Seo"
+                    showName="DJ MC Klaus and Oggy OG"
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Friday 3rd September"
+                />
+                <ScheduleItem
+                    showTime="18:00 - 19:00"
+                    showName="Charlie B"
+                />
+                <ScheduleItem
+                    showTime="19:00 - 20:00"
+                    showName="Marley"
+                />
+                <ScheduleItem
+                    showTime="20:00 - 21:00"
+                    showName="Doyle"
+                />
+                <ScheduleItem
+                    showTime="21:00 - 22:00"
+                    showName="Hannah O'Gorman"
+                />
+                <ScheduleItem
+                    showTime="22:00 - 23:00"
+                    showName="Great Danes"
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Saturday 4th September"
+                />
+                <ScheduleItem
+                    showTime="18:00 - 19:00"
+                    showName="Herbal Tea w/ George D"
+                />
+                <ScheduleItem
+                    showTime="19:00 - 20:00"
+                    showName="Fat Britney"
+                />
+                <ScheduleItem
+                    showTime="20:00 - 21:00"
+                    showName="Weedot."
+                />
+                <ScheduleItem
+                    showTime="21:00 - 22:00"
+                    showName="Jack Wood"
+                />
+                <ScheduleItem
+                    showTime="22:00 - 23:00"
+                    showName="Insurrect"
                 />
             </table>
         </div>
