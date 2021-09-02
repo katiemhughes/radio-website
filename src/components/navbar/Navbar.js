faf9ddbb69754b76a57e23e9d59d2568ca8bc1bf
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-09-02 21:58:05 +0100

    off air

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -112,11 +112,11 @@ const Navbar = () => {
               <ul>
                 <li><div className="circle red"></div></li>
                 {/* Write "ON" instead of "OFF" (or vice versa) */}
-                <li>ON AIR</li>
+                <li>OFF AIR</li>
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">DJ MC Klaus and Oggy OG</p>
+                  <p className="audio-player-text"></p>
                 </div>
               </ul>
             </div>
