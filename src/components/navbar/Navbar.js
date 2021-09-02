0c66dda2734d43336c9ece0ada3d0b2211d8fda1
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-09-02 19:00:20 +0100

    curl fm

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Acidhousedeathsquad</p>
+                  <p className="audio-player-text">Curl Fm</p>
               
                 </div>
               </ul>
