511690e9cfc2753b7213503844133ffb627e1e41
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-03 20:34:20 +0100

    great danes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Doyle</p>
+                  <p className="audio-player-text">Great Danes</p>
                 </div>
               </ul>
             </div>
