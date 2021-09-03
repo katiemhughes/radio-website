add37cfb5053e1cef8b4bde6ebfc452f484b3465
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-03 18:52:55 +0100

    git push

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">dig B b2b Pussy Stamina</p>
+                  <p className="audio-player-text">Marley</p>
                 </div>
               </ul>
             </div>
