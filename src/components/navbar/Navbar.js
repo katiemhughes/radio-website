39f72afd747ce577d3475e3443954daa5b92c4b8
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-04 22:56:47 +0100

    after radio

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Insurrect</p>
+                  <p className="audio-player-text">post radio shenanigans</p>
                 </div>
               </ul>
             </div>
