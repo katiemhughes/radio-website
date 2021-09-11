172ae536d6fd6b1399702c3a8f20f1e9653176cc
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-11 19:21:09 +0100

    face2face

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Salt Pillar</p>
+                  <p className="audio-player-text">Face2Face</p>
                 </div>
               </ul>
             </div>
