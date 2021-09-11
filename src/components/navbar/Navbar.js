8e4b2adf1c53354430a2d74b790519b7390fadde
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-11 21:56:29 +0100

    cam spen gibbin

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">DJ Project Manager</p>
+                  <p className="audio-player-text">Cam Spen b2b Gibbin</p>
                 </div>
               </ul>
             </div>
