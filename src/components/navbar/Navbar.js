b5f032fcf228c971185225bdc8f6676b7cf523af
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-11 20:51:02 +0100

    project manager

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Face2Face</p>
+                  <p className="audio-player-text">DJ Project Manager</p>
                 </div>
               </ul>
             </div>
