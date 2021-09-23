277673beb1ddb7b4c4e8bc6d56994182dc439217
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-09-23 18:00:12 +0100

    schedule update

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,77 +11,69 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 9th September"
+                    showDate="Thursday 23rd September"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Under The Umbrella"
+                    showName="Semi Peppered w/ EJ & Donnagan"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Animaux"
+                    showName="Radio Free Manchester"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Mouridis"
+                    showName="Codex"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Pussy Galore"
+                    showName="Under the Umbrella"
                 />
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Friday 10th September"
+                    showDate="Friday 24th September"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Static Interlude"
+                    showName="Shawn Cartier b2b Dufraine"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Aisling"
+                    showName="Medulasa"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="dig B"
+                    showName="Hannah O'Gorman"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Levi Love"
-                />
-                <ScheduleItem
-                    showTime="22:00 - 23:00"
-                    showName="Kultura Collective"
+                    showName="JavierJoeK"
                 />
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Saturday 11th September"
+                    showDate="Saturday 25th September"
                 />
                 <ScheduleItem
                     showTime="17:00 - 18:00"
-                    showName="Uncle Bill"
+                    showName="Elektraversion"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="HØLEIGH"
+                    showName="Ghoulish b2b Nova Cheq b2b Maracuya"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Salt Pillar"
+                    showName="Ghoulish b2b Nova Cheq b2b Maracuya"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Face2Face"
+                    showName="Bomabrd Groove"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="DJ Project Manager"
-                />
-                <ScheduleItem
-                    showTime="22:00 - 23:00"
-                    showName="Gibbin b2b Cam Spen"    
+                    showName="Bombard Groove"  
                 />
             </table>
         </div>
