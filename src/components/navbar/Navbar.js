2a05744e02d58dc09244c6279b72a2bfd6591935
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-10-01 21:06:45 +0100

    mephisto

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Atiké</p>
+                  <p className="audio-player-text">Mephisto</p>
                 </div>
               </ul>
             </div>
