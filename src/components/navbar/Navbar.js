fd2bde13db5d4a2bb3b10b5f457a8c6f2cd89cae
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-10-02 00:21:33 +0100

    maracuya

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Maracuya b2b Charlie B</p>
+                  <p className="audio-player-text">Maracuyas</p>
                 </div>
               </ul>
             </div>
