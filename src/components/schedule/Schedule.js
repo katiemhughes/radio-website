d284afb2272c7d1b39a48c13e2d5c980b7c62957
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-10-01 20:06:56 +0100

    bobo addition

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -71,6 +71,10 @@ const Schedule = () => {
                     showTime="21:00 - 22:00"
                     showName="Rose A"  
                 />
+                <ScheduleItem
+                    showTime="22:00 - 23:00"
+                    showName="Bobo"  
+                />
             </table>
         </div>
     )
