d38135ac6ccb2269e0055655ed9631cdb6227ca1
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-10-20 18:51:27 +0100

    removed google sheet

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,11 +8,6 @@ const Footer = () => {
                 <div className="left">
                     <img src="/images/footerlogo.png" alt="logo" className="logo" />
                 </div>
-                <div className="centre">
-                    <ul className="nav-menu">
-                        <li className="nav-item"><a href="https://docs.google.com/forms/d/e/1FAIpQLSe__9vaOxPR65MyM0XG7AqM4w9Ss6vYpzXOKESgS9Gec-ujpA/viewform" target="_blank" rel="noreferrer">Send a show proposal</a></li>
-                    </ul>
-                </div>
                 <div className="footer-right">
                 <ul className="footer-nav-menu">
                     <li className="nav-item">Artwork by <a href="https://www.crystallui.com/" target="_blank" rel="noreferrer">Crystal Lui</a></li>
