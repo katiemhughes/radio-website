da56b3f87e2ee66cdf8a414c87e28003c042d628
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-10-21 20:46:05 +0100

    Izzy Iz

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Semi Peppered</p>
+                  <p className="audio-player-text">Izzy Iz</p>
                 </div>
               </ul>
             </div>
