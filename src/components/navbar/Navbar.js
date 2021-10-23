2266115101754e9c490837d4756aecdb4e14bb41
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-10-23 19:03:06 +0100

    harry joe

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">dig-b</p>
+                  <p className="audio-player-text">DJ Simcard b2b harry amstel</p>
                 </div>
               </ul>
             </div>
