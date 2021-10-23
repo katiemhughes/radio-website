1e9d8aff37ac2b1d6267a1f8fe16f4735bf57773
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-10-23 20:43:24 +0100

    gina

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">DJ Simcard b2b harry amstel</p>
+                  <p className="audio-player-text">GINA the lucid juice queen</p>
                 </div>
               </ul>
             </div>
