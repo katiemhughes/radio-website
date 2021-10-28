ea306fc9e020c6dd0724ad3846374749ac6ad5b7
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-10-28 18:57:58 +0100

    earl grey

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Jess Rose</p>
+                  <p className="audio-player-text">Earl Grey</p>
                 </div>
               </ul>
             </div>
