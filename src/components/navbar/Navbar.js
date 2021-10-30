406157aa2d2caadc5b9718fb8f7bfe373e49f425
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-10-30 20:50:06 +0100

    hats

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Ellen</p>
+                  <p className="audio-player-text">Hats</p>
                 </div>
               </ul>
             </div>
