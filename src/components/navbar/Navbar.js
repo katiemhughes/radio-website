7868f974bc47378e2ee2054583714877d881d395
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-11-05 18:56:35 +0000

    liam oliver

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Elliott Lion</p>
+                  <p className="audio-player-text">Liam Oliver</p>
                 </div>
               </ul>
             </div>
