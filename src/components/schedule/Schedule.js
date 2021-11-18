edb90c393073755ad617aa3d5849ba8de45ad2fb
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-11-18 17:03:35 +0000

    schedule

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,49 +11,65 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 4th November"
+                    showDate="Thursday 18th November"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Sahar (DNA)"
+                    showName="Dylan James"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Ruby Swallow"
+                    showName="Izzy Iz"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Andrea Trout"
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Friday 5th November"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 22:00"
-                    showName="Elliott Lion b2b Liam Oliver b2b WeeDot. b2b Pangolin"
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Saturday 6th November"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="dig B"
-                />
-                <ScheduleItem
-                    showTime="19:00 - 20:00"
-                    showName="Charlie B"
-                />
-                <ScheduleItem
-                    showTime="20:00 - 21:00"
-                    showName="Maracuya"  
+                    showName="Herbal T w/ George D"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="LDV"  
+                    showName="Rizu X"
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Friday 19th November"
+                    />
+                    <ScheduleItem
+                        showTime="18:00 - 19:00"
+                        showName="EJ"
+                    />
+                    <ScheduleItem
+                        showTime="19:00 - 20:00"
+                        showName="VIEUX"
+                    />
+                    <ScheduleItem
+                        showTime="20:00 - 21:00"
+                        showName="Static Interlude"
+                    />
+                    <ScheduleItem
+                        showTime="21:00 - 22:00"
+                        showName="Léna C"
+        />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Saturday 20th November"
+                />
+                <ScheduleItem
+                    showTime="18:00 - 19:00"
+                    showName="Aitka Talo"
+                />
+                <ScheduleItem
+                    showTime="19:00 - 20:00"
+                    showName="Shannon From Admin"
+                />
+                <ScheduleItem
+                    showTime="20:00 - 21:00"
+                    showName="Doyle"  
+                />
+                <ScheduleItem
+                    showTime="21:00 - 22:00"
+                    showName="Mute Artefact w/ Alpine Sound"  
                 />
             </table>
         </div>
