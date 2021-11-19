469b8feed2e971655454ba99ea289d64af3d1547
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-11-19 19:55:43 +0000

    static interlude

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">VIEUX</p>
+                  <p className="audio-player-text">Static Interlude</p>
                 </div>
               </ul>
             </div>
