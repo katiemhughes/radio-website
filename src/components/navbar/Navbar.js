423eced3fa37ae92f37f8458955a916abd117215
katiemhughes <katie.m.hughes@hotmail.co.uk>
2021-11-26 18:55:58 +0000

    chesca

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">La Presidenta</p>
+                  <p className="audio-player-text">Pussy Stamina Presents High Voltage</p>
                 </div>
               </ul>
             </div>
