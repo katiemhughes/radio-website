1d34472b5e9a74bf6a1159a75f0d179881e35ae6
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-01-22 18:15:02 +0000

    sam

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -49,7 +49,7 @@ const Schedule = () => {
                 />
                 <ScheduleItem
                     showTime="18:00 - 20:00"
-                    showName="Yahuru & Capo"
+                    showName="Sam Carpenter"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
