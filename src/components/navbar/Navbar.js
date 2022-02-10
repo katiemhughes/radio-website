a3313cd508106c8118a79479d0b44118109fde64
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-02-10 20:00:49 +0000

    goeg

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Izzy Iz</p>
+                  <p className="audio-player-text">Herbal Tea w/ George D</p>
                 </div>
               </ul>
             </div>
