4d5c71a2541bc3609305620c30c0a54d42157f71
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-02-11 18:57:02 +0000

    mathew

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Tommy Cross</p>
+                  <p className="audio-player-text">Kiss Me Again</p>
                 </div>
               </ul>
             </div>
