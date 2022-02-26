5b6df386bee042bc9110a5415da4c887f687ae24
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-02-26 20:55:43 +0000

    dboy

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Aceca</p>
+                  <p className="audio-player-text">Dutty Boy Reflex</p>
                 </div>
               </ul>
             </div>
