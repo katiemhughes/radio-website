9f972650cef1859d1ea035727fb588fb537af86b
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-03-10 17:59:22 +0000

    thurs on

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -113,11 +113,11 @@ const Navbar = () => {
               <ul>
                 <li><div className="circle red"></div></li>
                 {/* Write "ON" instead of "OFF" (or vice versa) */}
-                <li>OFF AIR</li>
+                <li>ON AIR</li>
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text"></p>
+                  <p className="audio-player-text">Fat Britney</p>
                 </div>
               </ul>
             </div>
--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,49 +11,53 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 24th February"
+                    showDate="Thursday 10th March"
                 />
                 <ScheduleItem
-                    showTime="18:00 - 20:00"
-                    showName="MIRE"
-                />
-                <ScheduleItem
-                    showTime="20:00 - 21:00"
-                    showName="Relax and Bake FM"  
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Friday 25th February"
-                    />
-                    <ScheduleItem
-                        showTime="19:00 - 20:00"
-                        showName="Miss Bad B"
-                    />
-                    <ScheduleItem
-                        showTime="20:00 - 21:00"
-                        showName="Sam Carpenter"
-                    />
-                    <ScheduleItem
-                        showTime="21:00 - 22:00"
-                        showName= "Yahuru"
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Saturday 26th February"
+                    showTime="18:00 - 19:00"
+                    showName="Fat Britney"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Charlie B"
+                    showName="Izzy Iz"  
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Aceca"
+                    showName="Herbal Tea" 
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Friday 11th March"
+                    />
+                    <ScheduleItem
+                        showTime="18:00 - 19:00"
+                        showName="Felicia Buena"
+                    />
+                    <ScheduleItem
+                        showTime="19:00 - 20:00"
+                        showName="Tommy Cross"
+                    />
+                    <ScheduleItem
+                        showTime="20:00 - 21:00"
+                        showName= "Static Interlude w/ Jeneral Purpose"
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Saturday 12th March"
+                />
+                <ScheduleItem
+                    showTime="19:00 - 20:00"
+                    showName="Mouridis"
+                />
+                <ScheduleItem
+                    showTime="20:00 - 21:00"
+                    showName="Rose A"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Dutty Boy Reflex"  
+                    showName="Stolen Velour"  
                 />
             </table>
         </div>
