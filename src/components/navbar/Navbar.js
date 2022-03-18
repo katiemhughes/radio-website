7e82a1becd52e5e9c29cb11d9f9946fee0e8f8da
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-03-18 19:51:15 +0000

    alfaz

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">High Voltage</p>
+                  <p className="audio-player-text">Alfaz</p>
                 </div>
               </ul>
             </div>
