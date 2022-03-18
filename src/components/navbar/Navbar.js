a2937118fe38758ff1bf96d2009f2d725e16541d
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-03-18 21:00:16 +0000

    alex

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -113,11 +113,11 @@ const Navbar = () => {
               <ul>
                 <li><div className="circle red"></div></li>
                 {/* Write "ON" instead of "OFF" (or vice versa) */}
-                <li>ON AIR</li>
+                <li>OFF AIR</li>
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Alfaz</p>
+                  <p className="audio-player-text"></p>
                 </div>
               </ul>
             </div>
