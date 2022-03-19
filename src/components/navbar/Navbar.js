f7e564b43aecdb4e22d444a8f52ee049f524efa5
katiemhughes <katie.m.hughes@hotmail.co.uk>
2022-03-19 18:58:07 +0000

    hats

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Lady Marmaduke</p>
+                  <p className="audio-player-text">hats</p>
                 </div>
               </ul>
             </div>
